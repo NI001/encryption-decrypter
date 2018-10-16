5b4b289a46521ae6b031080477d8ffad6c05ecb0
Owen Kelly (KEL18140812) <KEL18140812@belfastmet.ac.uk>
2018-10-16 11:19:18 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.encrypt1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.encrypt2 = new System.Windows.Forms.Button();
+            this.encrypt3 = new System.Windows.Forms.Button();
+            this.decrypt3 = new System.Windows.Forms.Button();
+            this.decrypt2 = new System.Windows.Forms.Button();
+            this.decrypt1 = new System.Windows.Forms.Button();
+            this.encryption = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.decryption = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(83, 45);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(115, 20);
+            this.input.TabIndex = 0;
+            // 
+            // encrypt1
+            // 
+            this.encrypt1.Location = new System.Drawing.Point(7, 71);
+            this.encrypt1.Name = "encrypt1";
+            this.encrypt1.Size = new System.Drawing.Size(127, 23);
+            this.encrypt1.TabIndex = 1;
+            this.encrypt1.Text = "encrypt (Reversal)";
+            this.encrypt1.UseVisualStyleBackColor = true;
+            this.encrypt1.Click += new System.EventHandler(this.encrypt1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(66, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Encrypt / Decrypt";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // encrypt2
+            // 
+            this.encrypt2.Location = new System.Drawing.Point(7, 100);
+            this.encrypt2.Name = "encrypt2";
+            this.encrypt2.Size = new System.Drawing.Size(127, 23);
+            this.encrypt2.TabIndex = 3;
+            this.encrypt2.Text = "encrypt  (Replacement)";
+            this.encrypt2.UseVisualStyleBackColor = true;
+            this.encrypt2.Click += new System.EventHandler(this.encrypt2_Click);
+            // 
+            // encrypt3
+            // 
+            this.encrypt3.Location = new System.Drawing.Point(7, 129);
+            this.encrypt3.Name = "encrypt3";
+            this.encrypt3.Size = new System.Drawing.Size(127, 23);
+            this.encrypt3.TabIndex = 4;
+            this.encrypt3.Text = "encrypt  (Mixed)";
+            this.encrypt3.UseVisualStyleBackColor = true;
+            this.encrypt3.Click += new System.EventHandler(this.encrypt3_Click);
+            // 
+            // decrypt3
+            // 
+            this.decrypt3.Location = new System.Drawing.Point(140, 129);
+            this.decrypt3.Name = "decrypt3";
+            this.decrypt3.Size = new System.Drawing.Size(132, 23);
+            this.decrypt3.TabIndex = 7;
+            this.decrypt3.Text = "decrypt (Mixed)";
+            this.decrypt3.UseVisualStyleBackColor = true;
+            this.decrypt3.Click += new System.EventHandler(this.decrypt3_Click);
+            // 
+            // decrypt2
+            // 
+            this.decrypt2.Location = new System.Drawing.Point(140, 100);
+            this.decrypt2.Name = "decrypt2";
+            this.decrypt2.Size = new System.Drawing.Size(132, 23);
+            this.decrypt2.TabIndex = 6;
+            this.decrypt2.Text = "decrypt  (Replacement)";
+            this.decrypt2.UseVisualStyleBackColor = true;
+            this.decrypt2.Click += new System.EventHandler(this.decrypt2_Click);
+            // 
+            // decrypt1
+            // 
+            this.decrypt1.Location = new System.Drawing.Point(140, 71);
+            this.decrypt1.Name = "decrypt1";
+            this.decrypt1.Size = new System.Drawing.Size(132, 23);
+            this.decrypt1.TabIndex = 5;
+            this.decrypt1.Text = "decrypt  (Reversal)";
+            this.decrypt1.UseVisualStyleBackColor = true;
+            this.decrypt1.Click += new System.EventHandler(this.decrypt1_Click);
+            // 
+            // encryption
+            // 
+            this.encryption.Location = new System.Drawing.Point(83, 158);
+            this.encryption.Name = "encryption";
+            this.encryption.Size = new System.Drawing.Size(115, 20);
+            this.encryption.TabIndex = 8;
+            this.encryption.TextChanged += new System.EventHandler(this.encryption_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(4, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Encryption";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(4, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Decryption";
+            // 
+            // decryption
+            // 
+            this.decryption.Location = new System.Drawing.Point(83, 184);
+            this.decryption.Name = "decryption";
+            this.decryption.Size = new System.Drawing.Size(115, 20);
+            this.decryption.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(36, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Input";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.decryption);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.encryption);
+            this.Controls.Add(this.decrypt3);
+            this.Controls.Add(this.decrypt2);
+            this.Controls.Add(this.decrypt1);
+            this.Controls.Add(this.encrypt3);
+            this.Controls.Add(this.encrypt2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encrypt1);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button encrypt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button encrypt2;
+        private System.Windows.Forms.Button encrypt3;
+        private System.Windows.Forms.Button decrypt3;
+        private System.Windows.Forms.Button decrypt2;
+        private System.Windows.Forms.Button decrypt1;
+        private System.Windows.Forms.TextBox encryption;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox decryption;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string a;  // read strings from the UI
+        char[] entry, encrypted, decrypted, temp;
+        // encryption variables
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void encryption_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void encrypt2_Click(object sender, EventArgs e)
+        {
+            // reads user input and converts to a character array
+            a = this.input.Text;
+            entry = a.ToCharArray();
+            //set the sizes of other encryption variables
+            encrypted = new char[a.Length];
+            decrypted = new char[a.Length];
+            // substitution encryption
+            for (int i = 0; i < a.Length; i++)
+                switch (entry[i])
+                {
+                    case 'a':
+                        encrypted[i] = '1'; break;
+                    case 'b':
+                        encrypted[i] = '3'; break;
+                    case 'c':
+                        encrypted[i] = '5'; break;
+                    case 'd':
+                        encrypted[i] = '7'; break;
+                    case 'e':
+                        encrypted[i] = '9'; break;
+                    case 'f':
+                        encrypted[i] = '2'; break;
+                    case 'g':
+                        encrypted[i] = '4'; break;
+                    case 'h':
+                        encrypted[i] = '6'; break;
+                    case 'i':
+                        encrypted[i] = '8'; break;
+                    case 'j':
+                        encrypted[i] = '0'; break;
+                    case 'k':
+                        encrypted[i] = 'a'; break;
+                    case 'l':
+                        encrypted[i] = 'b'; break;
+                    case 'm':
+                        encrypted[i] = 'c'; break;
+                    case 'n':
+                        encrypted[i] = 'd'; break;
+                    case 'o':
+                        encrypted[i] = 'e'; break;
+                    case 'p':
+                        encrypted[i] = 'f'; break;
+                    case 'q':
+                        encrypted[i] = 'g'; break;
+                    case 'r':
+                        encrypted[i] = 'h'; break;
+                    case 's':
+                        encrypted[i] = 'i'; break;
+                    case 't':
+                        encrypted[i] = 'j'; break;
+                    case 'u':
+                        encrypted[i] = 'k'; break;
+                    case 'v':
+                        encrypted[i] = 'l'; break;
+                    case 'w':
+                        encrypted[i] = 'm'; break;
+                    case 'x':
+                        encrypted[i] = 'n'; break;
+                    case 'y':
+                        encrypted[i] = 'o'; break;
+                    case 'z':
+                        encrypted[i] = 'p'; break;
+                    case '1':
+                        encrypted[i] = 'q'; break;
+                    case '2':
+                        encrypted[i] = 'r'; break;
+                    case '3':
+                        encrypted[i] = 's'; break;
+                    case '4':
+                        encrypted[i] = 't'; break;
+                    case '5':
+                        encrypted[i] = 'u'; break;
+                    case '6':
+                        encrypted[i] = 'v'; break;
+                    case '7':
+                        encrypted[i] = 'w'; break;
+                    case '8':
+                        encrypted[i] = 'x'; break;
+                    case '9':
+                        encrypted[i] = 'y'; break;
+                    case '0':
+                        encrypted[i] = 'z'; break;
+                    case ' ':
+                        encrypted[i] = '/'; break;
+                    case '.':
+                        encrypted[i] = '<'; break;
+                }
+            this.encryption.Text = new string(encrypted);
+        }
+
+        private void decrypt1_Click(object sender, EventArgs e)
+        {
+            // reads user input and converts to a character array
+            //a = this.input.Text;
+            //entry = a.ToCharArray();
+            //set the sizes of other encryption variables
+
+            // reversal decrypition
+            for (int i = 0; i < a.Length; i++)
+            {
+                decrypted[i] = encrypted[entry.Length - 1 - i];
+
+            }
+            this.decryption.Text = new string(decrypted);
+        }
+
+        private void decrypt2_Click(object sender, EventArgs e)
+        {
+            // reads user input and converts to a character array
+            a = this.input.Text;
+            entry = a.ToCharArray();
+            //set the sizes of other encryption variables
+           // encrypted = new char[a.Length];
+           // decrypted = new char[a.Length];
+            // substitution decryption
+            for (int i = 0; i < a.Length; i++)
+                switch (encrypted[i])
+                {
+                    case 'a':
+                        decrypted[i] = 'k'; break;
+                    case 'b':
+                        decrypted[i] = 'l'; break;
+                    case 'c':
+                        decrypted[i] = 'm'; break;
+                    case 'd':
+                        decrypted[i] = 'n'; break;
+                    case 'e':
+                        decrypted[i] = 'o'; break;
+                    case 'f':
+                        decrypted[i] = 'p'; break;
+                    case 'g':
+                        decrypted[i] = 'q'; break;
+                    case 'h':
+                        decrypted[i] = 'r'; break;
+                    case 'i':
+                        decrypted[i] = 's'; break;
+                    case 'j':
+                        decrypted[i] = 't'; break;
+                    case 'k':
+                        decrypted[i] = 'u'; break;
+                    case 'l':
+                        decrypted[i] = 'v'; break;
+                    case 'm':
+                        decrypted[i] = 'w'; break;
+                    case 'n':
+                        decrypted[i] = 'x'; break;
+                    case 'o':
+                        decrypted[i] = 'y'; break;
+                    case 'p':
+                        decrypted[i] = 'z'; break;
+                    case 'q':
+                        decrypted[i] = '1'; break;
+                    case 'r':
+                        decrypted[i] = '2'; break;
+                    case 's':
+                        decrypted[i] = '3'; break;
+                    case 't':
+                        decrypted[i] = '4'; break;
+                    case 'u':
+                        decrypted[i] = '5'; break;
+                    case 'v':
+                        decrypted[i] = '6'; break;
+                    case 'w':
+                        decrypted[i] = '7'; break;
+                    case 'x':
+                        decrypted[i] = '8'; break;
+                    case 'y':
+                        decrypted[i] = '9'; break;
+                    case 'z':
+                        decrypted[i] = '0'; break;
+                    case '1':
+                        decrypted[i] = 'a'; break;
+                    case '2':
+                        decrypted[i] = 'f'; break;
+                    case '3':
+                        decrypted[i] = 'b'; break;
+                    case '4':
+                        decrypted[i] = 'g'; break;
+                    case '5':
+                        decrypted[i] = 'c'; break;
+                    case '6':
+                        decrypted[i] = 'h'; break;
+                    case '7':
+                        decrypted[i] = 'd'; break;
+                    case '8':
+                        decrypted[i] = 'i'; break;
+                    case '9':
+                        decrypted[i] = 'e'; break;
+                    case '0':
+                        decrypted[i] = 'j'; break;
+                    case '/':
+                        decrypted[i] = ' '; break;
+                    case '<':
+                        decrypted[i] = '.'; break;
+                }
+            this.decryption.Text = new string(decrypted);
+        }
+
+        private void decrypt3_Click(object sender, EventArgs e)
+        {
+            // reads user input and converts to a character array
+            a = this.input.Text;
+            entry = a.ToCharArray();
+            //set the sizes of other encryption variables
+            //encrypted = new char[a.Length];
+            //decrypted = new char[a.Length];
+            //temp = new char[a.Length];
+            // mixed decryption
+            for (int i = 0; i < entry.Length; i++)
+            {
+                temp[i] = encrypted[entry.Length - 1 - i];
+
+            }
+
+            for (int i = 0; i < a.Length; i++)
+                switch (temp[i])
+                {
+                    case 'a':
+                        temp[i] = 'k'; break;
+                    case 'b':
+                        temp[i] = 'l'; break;
+                    case 'c':
+                        temp[i] = 'm'; break;
+                    case 'd':
+                        temp[i] = 'n'; break;
+                    case 'e':
+                        temp[i] = 'o'; break;
+                    case 'f':
+                        temp[i] = 'p'; break;
+                    case 'g':
+                        temp[i] = 'q'; break;
+                    case 'h':
+                        temp[i] = 'r'; break;
+                    case 'i':
+                        temp[i] = 's'; break;
+                    case 'j':
+                        temp[i] = 't'; break;
+                    case 'k':
+                        temp[i] = 'u'; break;
+                    case 'l':
+                        temp[i] = 'v'; break;
+                    case 'm':
+                        temp[i] = 'w'; break;
+                    case 'n':
+                        temp[i] = 'x'; break;
+                    case 'o':
+                        temp[i] = 'y'; break;
+                    case 'p':
+                        temp[i] = 'z'; break;
+                    case 'q':
+                        temp[i] = '1'; break;
+                    case 'r':
+                        temp[i] = '2'; break;
+                    case 's':
+                        temp[i] = '3'; break;
+                    case 't':
+                        temp[i] = '4'; break;
+                    case 'u':
+                        temp[i] = '5'; break;
+                    case 'v':
+                        temp[i] = '6'; break;
+                    case 'w':
+                        temp[i] = '7'; break;
+                    case 'x':
+                        temp[i] = '8'; break;
+                    case 'y':
+                        temp[i] = '9'; break;
+                    case 'z':
+                        temp[i] = '0'; break;
+                    case '1':
+                        temp[i] = 'a'; break;
+                    case '2':
+                        temp[i] = 'f'; break;
+                    case '3':
+                        temp[i] = 'b'; break;
+                    case '4':
+                        temp[i] = 'g'; break;
+                    case '5':
+                        temp[i] = 'c'; break;
+                    case '6':
+                        temp[i] = 'h'; break;
+                    case '7':
+                        temp[i] = 'd'; break;
+                    case '8':
+                        temp[i] = 'i'; break;
+                    case '9':
+                        temp[i] = 'e'; break;
+                    case '0':
+                        temp[i] = 'j'; break;
+                    case '/':
+                        temp[i] = ' '; break;
+                    case '<':
+                        temp[i] = '.'; break;
+                }
+            this.decryption.Text = new string(temp);
+        }
+
+        private void encrypt3_Click(object sender, EventArgs e)
+        {
+            // reads user input and converts to a character array
+            a = this.input.Text;
+            entry = a.ToCharArray();
+            //set the sizes of other encryption variables
+            encrypted = new char[a.Length];
+            decrypted = new char[a.Length];
+            temp = new char[a.Length];
+            // mixed encryption
+            for (int i = 0; i < a.Length; i++)
+            {
+                temp[i] = entry[entry.Length - 1 - i];
+
+            }
+
+            for (int i = 0; i < a.Length; i++)
+                switch (temp[i])
+                {
+                    case 'a':
+                        temp[i] = '1'; break;
+                    case 'b':
+                        temp[i] = '3'; break;
+                    case 'c':
+                        temp[i] = '5'; break;
+                    case 'd':
+                        temp[i] = '7'; break;
+                    case 'e':
+                        temp[i] = '9'; break;
+                    case 'f':
+                        temp[i] = '2'; break;
+                    case 'g':
+                        temp[i] = '4'; break;
+                    case 'h':
+                        temp[i] = '6'; break;
+                    case 'i':
+                        temp[i] = '8'; break;
+                    case 'j':
+                        temp[i] = '0'; break;
+                    case 'k':
+                        temp[i] = 'a'; break;
+                    case 'l':
+                        temp[i] = 'b'; break;
+                    case 'm':
+                        temp[i] = 'c'; break;
+                    case 'n':
+                        temp[i] = 'd'; break;
+                    case 'o':
+                        temp[i] = 'e'; break;
+                    case 'p':
+                        temp[i] = 'f'; break;
+                    case 'q':
+                        temp[i] = 'g'; break;
+                    case 'r':
+                        temp[i] = 'h'; break;
+                    case 's':
+                        temp[i] = 'i'; break;
+                    case 't':
+                        temp[i] = 'j'; break;
+                    case 'u':
+                        temp[i] = 'k'; break;
+                    case 'v':
+                        temp[i] = 'l'; break;
+                    case 'w':
+                        temp[i] = 'm'; break;
+                    case 'x':
+                        temp[i] = 'n'; break;
+                    case 'y':
+                        temp[i] = 'o'; break;
+                    case 'z':
+                        temp[i] = 'p'; break;
+                    case '1':
+                        temp[i] = 'q'; break;
+                    case '2':
+                        temp[i] = 'r'; break;
+                    case '3':
+                        temp[i] = 's'; break;
+                    case '4':
+                        temp[i] = 't'; break;
+                    case '5':
+                        temp[i] = 'u'; break;
+                    case '6':
+                        temp[i] = 'v'; break;
+                    case '7':
+                        temp[i] = 'w'; break;
+                    case '8':
+                        temp[i] = 'x'; break;
+                    case '9':
+                        temp[i] = 'y'; break;
+                    case '0':
+                        temp[i] = 'z'; break;
+                    case ' ':
+                        temp[i] = '/'; break;
+                    case '.':
+                        temp[i] = '<'; break;
+                }
+            for (int i = 0; i < temp.Length; i++)
+            { encrypted[i] = temp[i]; }
+
+            this.encryption.Text = new string(temp);
+        }
+
+        private void encrypt1_Click(object sender, EventArgs e)
+        {
+            // reads user input and converts to a character array
+            a = this.input.Text;
+            entry = a.ToCharArray();
+            //set the sizes of other encryption variables
+            encrypted = new char[a.Length];
+            decrypted = new char[a.Length];
+            // reversal encryption
+            for (int i = 0; i < a.Length; i++)
+            {
+                encrypted[i] = entry[entry.Length-1-i];
+
+            }
+            this.encryption.Text = new string(encrypted);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
